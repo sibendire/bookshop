bab45334ae6b347b65b4fb734c2b28daee60eef2
JOSH <sibendirejoshua@gmail.com>
2023-08-29 15:12:41 +0300

    Updated bookservice class

--- src/main/java/com/bookshop/bookshopmanagement/service/BookService.java
+++ src/main/java/com/bookshop/bookshopmanagement/service/BookService.java
@@ -1,8 +1,7 @@
 package com.bookshop.bookshopmanagement.service;
 
 import com.bookshop.bookshopmanagement.entity.Book;
-import com.bookshop.bookshopmanagement.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 @Service
