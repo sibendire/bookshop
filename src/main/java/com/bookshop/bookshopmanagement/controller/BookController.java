6c4e5e1c71df4f1ee7bee01fe0e1cde96f9d18ac
JOSH <sibendirejoshua@gmail.com>
2023-09-07 23:41:34 +0300

    added ui.model

--- src/main/java/com/bookshop/bookshopmanagement/controller/BookController.java
+++ src/main/java/com/bookshop/bookshopmanagement/controller/BookController.java
@@ -1,19 +1,20 @@
 package com.bookshop.bookshopmanagement.controller;
 
 
-import ch.qos.logback.core.model.Model;
 import com.bookshop.bookshopmanagement.entity.Book;
 import com.bookshop.bookshopmanagement.entity.MyBook;
 import com.bookshop.bookshopmanagement.service.BookService;
 import com.bookshop.bookshopmanagement.service.MyBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+
 
 @Controller
 public class BookController {
