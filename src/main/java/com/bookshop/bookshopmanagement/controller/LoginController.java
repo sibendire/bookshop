0b08e351a6b32c2eeb648462eb4f1f93b9eb2c70
JOSH <sibendirejoshua@gmail.com>
2023-09-13 10:57:21 +0300

    updated login controller

--- src/main/java/com/bookshop/bookshopmanagement/controller/LoginController.java
+++ src/main/java/com/bookshop/bookshopmanagement/controller/LoginController.java
@@ -2,12 +2,13 @@ package com.bookshop.bookshopmanagement.controller;
 
 import com.bookshop.bookshopmanagement.service.LoginService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
     private LoginService loginService;
-
+@GetMapping("/login")
     public String loginSuccess() {
-        return "loginSuccess";
+        return "login";
     }
 }
