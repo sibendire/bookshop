91a76069b02987a0541a0c88ac2a9f392292eb08
JOSH <sibendirejoshua@gmail.com>
2023-10-06 10:00:50 +0300

    Added  return statements

--- src/main/java/com/bookshop/bookshopmanagement/entity/AccountDetails.java
+++ src/main/java/com/bookshop/bookshopmanagement/entity/AccountDetails.java
@@ -13,31 +13,31 @@ public class AccountDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
